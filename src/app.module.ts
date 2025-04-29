006e10cf13717d78e16fd037a5d97df0436a3826
cristian1014 <cristiandavidgf@gmail.com>
2025-04-29 14:03:37 -0500

    feat: add TypeORM and PostgreSQL support

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { EquipoModule } from './equipo/equipo.module';
 import { VideojuegoModule } from './videojuego/videojuego.module';
 import { EstudianteModule } from './estudiante/estudiante.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [EquipoModule, VideojuegoModule, EstudianteModule],
+  imports: [
+    EquipoModule, 
+    VideojuegoModule, 
+    EstudianteModule,
+    TypeOrmModule.forRoot({}),
+  ],
   providers: [],
 })
 export class AppModule {}
