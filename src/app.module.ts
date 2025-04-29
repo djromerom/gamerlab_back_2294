e175a9e4a60862c20c28dd6a14ee8292f66ec52c
cristian1014 <cristiandavidgf@gmail.com>
2025-04-29 13:30:05 -0500

    added support for Swagger in the API

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppService } from './app.service';
 import { EquipoModule } from './equipo/equipo.module';
 import { VideojuegoModule } from './videojuego/videojuego.module';
 import { EstudianteModule } from './estudiante/estudiante.module';
 
 @Module({
   imports: [EquipoModule, VideojuegoModule, EstudianteModule],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Feria Gamer API')
+    .setDescription('The Feria Gamer API description')
+    .setVersion('0.1')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
