99ca2039d060da7678fe88cb4866def4bdb76498
cristian1014 <cristiandavidgf@gmail.com>
2025-05-01 13:52:16 -0500

    refactor: remove TypeORM dependencies and configuration from the project

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,24 +2,12 @@ import { Module } from '@nestjs/common';
 import { EquipoModule } from './modules/equipo/equipo.module';
 import { VideojuegoModule } from './modules/videojuego/videojuego.module';
 import { EstudianteModule } from './modules/estudiante/estudiante.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     EquipoModule, 
     VideojuegoModule, 
     EstudianteModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT || '5432', 10),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      autoLoadEntities: true,
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true, // Set to false in production
-    }),
   ],
   providers: [],
 })
