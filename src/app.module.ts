41441f4f697e90c236898d26049aa64e617b2f5d
cristian <cristiandavidgf@gmail.com>
2025-05-03 16:37:53 -0500

    feat: remove EstudianteModule from app module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,11 @@
 import { Module } from '@nestjs/common';
 import { EquipoModule } from './modules/equipo/equipo.module';
 import { VideojuegoModule } from './modules/videojuego/videojuego.module';
-import { EstudianteModule } from './modules/estudiante/estudiante.module';
-import { PrismaService } from './prisma/prisma.service'
 
 @Module({
   imports: [
     EquipoModule, 
     VideojuegoModule, 
-    EstudianteModule,
   ],
   providers: [],
 })
