b7b9ff2b1260286f71cbf1d908ca74ce3296cdf6
cristian <cristiandavidgf@gmail.com>
2025-05-03 16:37:20 -0500

    feat: implement estudiante creation and management with validation and DTO enhancements

--- src/modules/estudiante/entities/estudiante.entity.ts
+++ src/modules/estudiante/entities/estudiante.entity.ts
@@ -1,1 +1,28 @@
-export class Estudiante {}
+import { Estudiante } from "@prisma/client";
+import { ApiProperty } from "@nestjs/swagger";
+
+type EstudianteInterface = Omit<Estudiante, 'deleted'>;
+
+
+export class EstudianteEntity {
+  @ApiProperty()
+  id: number;
+
+  @ApiProperty()
+  equipo_id: number;
+
+  @ApiProperty()
+  id_user: number;
+
+  @ApiProperty()
+  github: string;
+
+  @ApiProperty()
+  confirmado: boolean;
+
+  @ApiProperty()
+  create_at: Date;
+
+  @ApiProperty()
+  update_at: Date;
+}
