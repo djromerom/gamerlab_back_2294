161ef71ef26b2bb21bb140d5f3b2aae4cb142f21
cristian1014 <cristiandavidgf@gmail.com>
2025-05-04 14:58:00 -0500

    feat: add NRC and materia module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,16 @@ import { EquipoModule } from './modules/equipo/equipo.module';
 import { VideojuegoModule } from './modules/videojuego/videojuego.module';
 import { EstudianteModule } from './modules/estudiante/estudiante.module';
 import { PrismaService } from './prisma/prisma.service'
+import { MateriaModule } from './modules/materia/materia.module';
+import { NrcModule } from './modules/nrc/nrc.module';
 
 @Module({
   imports: [
     EquipoModule, 
     VideojuegoModule, 
     EstudianteModule,
+    MateriaModule,
+    NrcModule,
   ],
   providers: [],
 })
