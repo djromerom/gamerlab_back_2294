7c6365bab7172a8e01784a3fb4b059b98b336efc
Andrs08 <chinchillahuelvas@gmail.com>
2025-05-09 13:11:23 -0500

    feat: creacion de archivo main-evaluacion para ejecutar el modulo evaluacion y archivo seed para hacer querys

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,5 @@
 import { Module } from '@nestjs/common';
-import { EquipoModule } from './modules/equipo/equipo.module';
-import { VideojuegoModule } from './modules/videojuego/videojuego.module';
 import { ConfigModule } from '@nestjs/config'
-import { EstudianteModule } from './modules/estudiante/estudiante.module';
-import { MateriaModule } from './modules/materia/materia.module';
-import { NrcModule } from './modules/nrc/nrc.module';
-import { StorageModule } from './modules/storage/storage.module'
 import { EvaluacionModule } from './modules/evaluacion/evaluacion.module'
 
 @Module({
@@ -15,11 +9,6 @@ import { EvaluacionModule } from './modules/evaluacion/evaluacion.module'
       envFilePath: '.env',
     }),
     EvaluacionModule,
-    /*EquipoModule, 
-    VideojuegoModule, 
-    EstudianteModule,
-    MateriaModule,
-    NrcModule,*/
   ],
   providers: [],
 })
